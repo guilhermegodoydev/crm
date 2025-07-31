f576961a1a82173a9f0aad7ac38e7aad4220c72f
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2025-07-31 19:28:35 -0300

    Removendo autenticação

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Auth from "./pages/Auth.jsx";
+import App from "./App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Auth />
+    <App />
   </StrictMode>
 );
--- src/pages/Auth.jsx
+++
@@ -1,5 +0,0 @@
-import FormLogin from "../components/formLogin";
-
-export default function Auth() {
-  return <FormLogin />;
-}

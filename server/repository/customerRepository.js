870c476221ca29c833eb3c0259a8a27aeadb288b
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2025-08-06 20:05:53 -0300

    chore: alterando arquivos conteudo

--- server/repository/customerRepository.js
+++ server/repository/customerRepository.js
@@ -1,11 +1,1 @@
-import { pool } from "../db/db.js";
 
-export const selectAll = async () => {
-  try {
-    const res = await pool.query("SELECT * FROM customer");
-    return res.rows;
-  } catch (error) {
-    console.error("Erro ao buscar usuarios", error);
-    throw error;
-  }
-};

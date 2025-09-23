898ff87327ababfc41c8d4ec425ffc9db84b5edc
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2025-09-23 15:34:18 -0300

    corrgindo - header ficando atras do conteúdo

--- client/src/components/MenuLateral.jsx
+++ client/src/components/MenuLateral.jsx
@@ -9,7 +9,7 @@ export default function MenuLateral() {
     return (
         <div>
             <button aria-label='Clique para Abrir o Menu' onClick={() => setVisivel(!visivel)}><Menu className="m-2"/></button>
-            <nav className={`bg-blue-500 w-1/4 h-screen absolute top-0 p-2 transition-all ease-out transform ${ visivel ? "translate-x-0 shadow-black shadow-xl" : "-translate-x-full"}`}>
+            <nav className={`z-1 bg-blue-500 w-1/4 h-screen absolute top-0 p-2 transition-all ease-out transform ${ visivel ? "translate-x-0 shadow-black shadow-xl" : "-translate-x-full"}`}>
                 <ul>
                     <li onClick={() => setVisivel(!visivel)}>
                         <ArrowLeft className="text-white" />

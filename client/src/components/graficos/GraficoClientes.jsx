47cfb4782c38274f3968f2f3daaa90abc10b1bd5
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2025-09-25 20:50:37 -0300

    Adicionando taxa de crescimento

--- client/src/components/graficos/GraficoClientes.jsx
+++ client/src/components/graficos/GraficoClientes.jsx
@@ -1,24 +1,30 @@
 import {ResponsiveContainer, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from "recharts";
 
 const dados = [
-    { nome: "Janeiro", novosClientes: 400, regular: 200, premium: 200 },
-    { nome: "Fevereiro", novosClientes: 600, regular: 500, premium: 100 },
-    { nome: "Março", novosClientes: 800, regular: 300, premium: 500 },
-    { nome: "Abril", novosClientes: 700, regular: 400, premium: 300 },
-    { nome: "Maio", novosClientes: 500, regular: 200, premium: 300 },
-    { nome: "Junho", novosClientes: 400, regular: 100, premium: 300 },
-    { nome: "Julho", novosClientes: 600, regular: 400, premium: 200 },
-    { nome: "Agosto", novosClientes: 700, regular: 300, premium: 400 },
-    { nome: "Setembro", novosClientes: 800, regular: 500, premium: 300 },
-    { nome: "Outubro", novosClientes: 900, regular: 600, premium: 300 },
-    { nome: "Novembro", novosClientes: 1000, regular: 700, premium: 300 },
-    { nome: "Dezembro", novosClientes: 1100, regular: 800, premium: 300 },
+    { nome: "Jan", novosClientes: 400, regular: 200, premium: 200 },
+    { nome: "Fev", novosClientes: 600, regular: 500, premium: 100 },
+    { nome: "Mar", novosClientes: 800, regular: 300, premium: 500 },
+    { nome: "Abr", novosClientes: 700, regular: 400, premium: 300 },
+    { nome: "Mai", novosClientes: 500, regular: 200, premium: 300 },
+    { nome: "Jun", novosClientes: 400, regular: 100, premium: 300 },
+    { nome: "Jul", novosClientes: 600, regular: 400, premium: 200 },
+    { nome: "Ago", novosClientes: 700, regular: 300, premium: 400 },
+    { nome: "Set", novosClientes: 800, regular: 500, premium: 300 },
+    { nome: "Out", novosClientes: 900, regular: 600, premium: 300 },
+    { nome: "Nov", novosClientes: 1000, regular: 700, premium: 300 },
+    { nome: "Dez", novosClientes: 1100, regular: 800, premium: 300 },
 ];
 
 export default function GraficoClientes({ className }) {
+
+    const taxaCrescimento = 15;
+    const taxaPositiva = taxaCrescimento >= 0;
+    const taxaFormatada = `${taxaPositiva ? '+' : ''} ${Math.abs(taxaCrescimento)}%`;
+
     return (
         <div className={`h-75 w-full ${className}`}>
             <h1>Gráfico de Novos Clientes</h1>
+            <p className={taxaPositiva ? "text-green-500" : "text-red-500"}>Taxa de Crescimento: {taxaFormatada}</p>
             <ResponsiveContainer>
                 <LineChart data={dados}>
                     <CartesianGrid strokeDasharray="3 3"/>

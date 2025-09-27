a857f4a873a8910932495022930bda964f063b4c
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2025-09-26 22:31:28 -0300

    alterando para gráfico de barras

--- client/src/components/graficos/Funil.jsx
+++ client/src/components/graficos/Funil.jsx
@@ -1,26 +1,27 @@
-import { Bar, BarChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+import { Bar, BarChart, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
 
 const dados = [
-    { nome: 'Lead Capturado', quantidade: 4000 },
-    { nome: 'Contato', quantidade: 3000 },
-    { nome: 'Proposta', quantidade: 2000 },
-    { nome: 'Negociação', quantidade: 2780 },
-    { nome: 'Fechamento ganho', quantidade: 1890 },
-    { nome: 'Perdido', quantidade: 2390 },
+    { nome: 'Lead Capturado', regular: 4000, premium: 2400, },
+    { nome: 'Contato', regular: 3000, premium: 1398, },
+    { nome: 'Proposta', regular: 2000, premium: 9800, },
+    { nome: 'Negociação', regular: 2780, premium: 3908, },
+    { nome: 'Fechamento ganho', regular: 1890, premium: 4800, },
+    { nome: 'Perdido', regular: 2390, premium: 3800, },
 ];
 
 export default function Funil({ className }) {
     return (
         <div className={className}>
             <ResponsiveContainer>
-                <BarChart data={dados}>
-                    <CartesianGrid strokeDasharray="3 3"/>
-                    <XAxis dataKey="nome"/>
-                    <YAxis dataKey="quantidade"/>
-                    <Bar dataKey="quantidade" />
+                <BarChart data={dados} layout="vertical">
+                    <XAxis type="number"/>
+                    <YAxis dataKey="nome" type="category"/>
+                    <Bar dataKey="regular" stackId="a" fill="#4400ffff"/>
+                    <Bar dataKey="premium" stackId="a" fill="#00aeffff"/>
                     <Tooltip/>
+                    <Legend/>
                 </BarChart>
-            </ResponsiveContainer>
+            </ResponsiveContainer>          
         </div>
     );
 }

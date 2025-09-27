0c86f0c8bc303d7534f1b2af8f63266074e6fef3
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2025-09-26 22:32:12 -0300

    Adicionando navegação entra páginas

--- client/src/components/MenuLateral.jsx
+++ client/src/components/MenuLateral.jsx
@@ -1,29 +1,25 @@
-import { ChartNoAxesCombined, UsersRound, ArrowLeft, Menu } from 'lucide-react';
-import { useState } from 'react';
 
-const estiloElementos = "flex gap-4 cursor-pointer border-b border-white text-white p-2 hover:bg-blue-700 hover:font-bold";
+
+import { ChartNoAxesCombined, UsersRound, } from 'lucide-react';
+import { Link } from 'react-router-dom';
+
+const estiloElementos = "cursor-pointer text-white p-2 hover:bg-blue-700 hover:font-bold";
 
 export default function MenuLateral() {
-    const [visivel, setVisivel] = useState(false);
-
     return (
-        <div>
-            <button aria-label='Clique para Abrir o Menu' onClick={() => setVisivel(!visivel)}><Menu className="m-2"/></button>
-            <nav className={`z-1 bg-blue-500 w-1/4 h-screen absolute top-0 p-2 transition-all ease-out transform ${ visivel ? "translate-x-0 shadow-black shadow-xl" : "-translate-x-full"}`}>
-                <ul>
-                    <li onClick={() => setVisivel(!visivel)}>
-                        <ArrowLeft className="text-white" />
-                    </li>
-                    <li className={estiloElementos}>
+        <nav className={`bg-blue-500 w-min h-screen top-0 p-2`}>
+            <ul>
+                <li className={estiloElementos}>
+                    <Link to="/">
                         <ChartNoAxesCombined />
-                        DashBoard
-                    </li>
-                    <li className={estiloElementos}>    
+                    </Link>
+                </li>
+                <li className={estiloElementos}>
+                    <Link to="/clientes">
                         <UsersRound />
-                        Clientes
-                    </li>
-                </ul>
-            </nav>
-        </div>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
     )   
 }

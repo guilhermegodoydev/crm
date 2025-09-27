58fcfc6d613063d27771cb6a624112d0e7875d67
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2025-09-26 22:44:42 -0300

    criando página dashboard

--- client/src/screens/Dashboard.jsx
+++ client/src/screens/Dashboard.jsx
@@ -1,13 +1,16 @@
+import Card from "../components/Card";
+import Funil from "../components/graficos/Funil";
 import GraficoClientes from "../components/graficos/GraficoClientes";
 import GraficoOrigemClientes from "../components/graficos/GraficoOrigemClientes";
 
 export default function Dashboard() {
     return (
-        <main>
-            <div className="grid grid-cols-3 grid-rows-auto">
-                <GraficoClientes className="col-span-2"/>
-                <GraficoOrigemClientes/>
-            </div>
-        </main>
+        <div className="grid grid-cols-3 grid-rows-2 gap-15">
+            <Card titulo="Novos Clientes" valor="1,250" taxaCrescimento={15}/>
+            <Card titulo="Clientes Perdidos" valor="200" taxaCrescimento={-5}/>
+            <Funil className="col-span-1"/>
+            <GraficoClientes className="col-span-2"/>
+            <GraficoOrigemClientes/>
+        </div>
     );
 }

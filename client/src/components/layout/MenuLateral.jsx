dd7a5167b9745adefc12c18df8b511523aeecb89
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2025-10-13 16:59:11 -0300

    Alterando html para melhor semântica

--- client/src/components/layout/MenuLateral.jsx
+++ client/src/components/layout/MenuLateral.jsx
@@ -5,24 +5,26 @@ const estiloElementos = "cursor-pointer text-white p-2 hover:bg-blue-700 hover:f
 
 export default function MenuLateral() {
     return (
-        <nav className={`bg-blue-500 w-min h-screen top-0 p-2`}>
-            <ul>
-                <li className={estiloElementos}>
-                    <Link to="/" aria-label="Clique para ir à página de dashboar">
-                        <ChartNoAxesCombined />
-                    </Link>
-                </li>
-                <li className={estiloElementos}>
-                    <Link to="/clientes" aria-label="Clique para ir à página de clientes">
-                        <UsersRound />
-                    </Link>
-                </li>
-                <li className={estiloElementos}>
-                    <Link to="/etapas" aria-label="Clique para ir à página de etapas">
-                        <Layers/>
-                    </Link>
-                </li>
-            </ul>
-        </nav>
+        <aside className={`bg-blue-500 w-min h-screen top-0 p-2`}>
+            <nav>
+                <ul>
+                    <li className={estiloElementos}>
+                        <Link to="/" aria-label="Clique para ir à página de dashboar">
+                            <ChartNoAxesCombined />
+                        </Link>
+                    </li>
+                    <li className={estiloElementos}>
+                        <Link to="/clientes" aria-label="Clique para ir à página de clientes">
+                            <UsersRound />
+                        </Link>
+                    </li>
+                    <li className={estiloElementos}>
+                        <Link to="/etapas" aria-label="Clique para ir à página de etapas">
+                            <Layers/>
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </aside>
     )   
 }

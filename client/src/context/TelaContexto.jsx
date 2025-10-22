7cdcef53a97f3ea6604c877e56805c6831f97626
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2025-10-22 17:12:22 -0300

    Adicionando debounce

--- client/src/context/TelaContexto.jsx
+++ client/src/context/TelaContexto.jsx
@@ -6,8 +6,13 @@ export function TelaProvider({ children }) {
     const [desktop, setDesktop] = useState(window.innerWidth >= 1024);
 
     useEffect(() => {
-        const handleResize = () => setDesktop(window.innerWidth >= 1024);
-        setTimeout(handleResize, 0);
+        let tempo = null;
+        const handleResize = () => {
+            clearTimeout(tempo);
+            tempo = setTimeout(() => {
+                setDesktop(window.innerWidth >= 1024);
+            }, 150);
+        };
         window.addEventListener("resize", handleResize);
         return () => window.removeEventListener("resize", handleResize);
     }, []);

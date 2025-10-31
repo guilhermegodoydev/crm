1dc719c36879721f65e947f06df2bc1c2d2635a5
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2025-10-31 19:30:47 -0300

    Resolvendo bug de clientes vazios

--- client/src/hooks/useCliente.jsx
+++ client/src/hooks/useCliente.jsx
@@ -3,25 +3,25 @@ import { useFetch } from "./useFetch";
 import { useLocalStorage } from "./useLocalStorage";
 
 export function useCliente() {
-    const { mock: dados, carregando, erro } = useFetch("/mock/clientes.json");
-    const [ valor, setValor ] = useLocalStorage("clientes", []);
+    const { dados: mock, carregando, erro } = useFetch("/mock/clientes.json");
+    const [ clientes, setClientes ] = useLocalStorage("clientes", []);
 
     useEffect(() => {
-        if (!carregando && !erro && valor.length === 0)
-            setValor(mock);
-    }, [mock, valor]);
+        if (!carregando && !erro && clientes.length === 0 && mock)
+            setClientes(mock);
+    }, [mock, carregando, erro]);
 
     const criar  = (cliente) => {
-        setValor(prev => [...prev, { id: crypto.randomUUID(), ...cliente}]);
+        setClientes(prev => [...prev, { id: crypto.randomUUID(), ...cliente}]);
     };
 
-    const alterar = (cliente) => {
-        setValor(prev => prev.map(c => c.id === cliente.id ? {...c, ...cliente} : c));
+    const atualizar = (cliente) => {
+        setClientes(prev => prev.map(c => c.id === cliente.id ? {...c, ...cliente} : c));
     };
 
     const remover = (clienteId) => {
-        setValor(prev => prev.filter(c => c.id !== clienteId));
+        setClientes(prev => prev.filter(c => c.id !== clienteId));
     };
 
-    return { clientes: valor, carregando, erro, criar, alterar, remover };
+    return { clientes, carregando, erro, criar, atualizar, remover };
 }

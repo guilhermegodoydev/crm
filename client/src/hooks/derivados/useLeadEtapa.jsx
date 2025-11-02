dc585d179d73f9b04c8aa926045358cb101eb386
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2025-11-02 18:33:16 -0300

    Criando função para mover lead de etapa

--- client/src/hooks/derivados/useLeadEtapa.jsx
+++ client/src/hooks/derivados/useLeadEtapa.jsx
@@ -1,15 +1,28 @@
+import { useCallback, useMemo } from "react";
+
 import { useEtapa } from "../dominio/useEtapa";
+import { useLead } from "../dominio/useLead";
 
 export function useLeadEtapa() {
     const { etapas, carregando: caEtapas, erro: erEtapas } = useEtapa();
-    const { leads, carregando: catLeads, erro: erLeads } = useLeads(); 
-
+    const { leads, atualizar: atualizarLead, carregando: caLeads, erro: erLeads } = useLead(); 
+    
     const carregando = caEtapas || caLeads;
     const erro = erEtapas || erLeads;
 
-    const listarLeadsPorEtapa = (etapaId) => {
-        return leads.filter(l => l.etapa == etapaId);
-    };
+    const leadsPorEtapa = useMemo(() => {
+        return etapas.map(e => ({
+            ...e,
+            leads: leads.filter(l => l.etapa == e.id)
+        }));
+    }, [etapas, leads]);
 
-    return { listarLeadsPorEtapa, carregando, erro};
+    const moverLeadParaEtapa = useCallback((leadId, etapaId) => {
+        const lead = leads.find(l => l.id == leadId);
+        if (!lead || lead.etapa == etapaId) return;
+
+        atualizarLead(leadId, { etapa: etapaId});
+    }, [leads, atualizarLead]);
+
+    return { leadsPorEtapa, moverLeadParaEtapa, carregando, erro};
 }

b29228c3b0a5a32f3f609fe0371d535986b663b1
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2025-11-02 16:15:26 -0300

    Criando hook de dominio para entidade etapa

---
+++ client/src/hooks/useEtapa.jsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+
+import { useFetch } from "./useFetch";
+import { useLocalStorage } from "./useLocalStorage";
+
+export function useEtapa() {
+    const { dados: mock, carregando, erro } = useFetch("/mock/etapas.json");
+    const [ etapas, setEtapas ] = useLocalStorage();
+
+    useEffect(() => {
+        if (!carregando && erro && etapas.length === 0 && mock) 
+            setEtapas(mock);
+    }, [mock, carregando, erro]);
+
+    const criar = (etapa) => {
+        setEtapas(prev => [...prev, { id: crypto.randomUUID(), ...etapa}]);
+    };
+
+    const atualizar = (etapa) => {
+        setEtapas(prev => prev.map(e => e.id == etapa.id ? {...e, ...etapa} : c));
+    };
+
+    const remover = (etapaId) => {
+        setEtapas(prev => prev.filter(e => e.id != etapaId));
+    };
+
+    const buscar = (etapaId) => {
+        return etapas.find(e => e.id === etapaId);
+    };
+
+    return { etapas, carregando, erro, criar, atualizar, remover, buscar };
+}

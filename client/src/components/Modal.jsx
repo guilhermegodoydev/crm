746c17cb35f03d36e4b88799e651b11adc50f877
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2025-11-16 13:54:16 -0300

    Alterando modal

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,20 +1,30 @@
+import { createPortal } from "react-dom";
 import { Card } from "./Card";
 import { X } from "lucide-react";
 
 export function Modal({ titulo, mensagem, className = "", children , aberto, onFechar }) {
+    const modalRoot = document.getElementById("modal-root");
+
+    if (!modalRoot) return null;
     if (!aberto) return null;
 
-    return (
-        <Card className={`w-100 absolute left-[50%] top-[50%] translate-[-50%] bg-white ${className}`}>
-            <div className="flex justify-between items-center">
-                <h2>{titulo ?? 'Aviso'}</h2>
-                <X onClick={onFechar} className="cursor-pointer" />
-            </div>
-            <hr className="border-gray-300" />
-            <div className="py-5">
-                {mensagem}
-            </div>
-            {children}
-        </Card>
+    return createPortal(
+        <div className="fixed inset-0 z-50 bg-gray-700/80 flex items-center justify-center">
+            <Card className={`relative w-100 bg-white ${className}`}>
+                <div className="flex justify-between items-center">
+                    <h2>{titulo ?? 'Aviso'}</h2>
+                    <X onClick={onFechar} className="cursor-pointer" />
+                </div>
+
+                <hr className="border-gray-300" />
+
+                <div className="py-5">
+                    {mensagem}
+                </div>
+
+                {children}
+            </Card>
+        </div>,
+        modalRoot
     );
 }

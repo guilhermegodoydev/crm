3062f80b19d02b69a63896de45150ab2075766ce
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2025-11-16 17:08:14 -0300

    Adicionando resumo de relacionamento com cliente

--- client/src/screens/clientes/ClienteDetalhes.jsx
+++ client/src/screens/clientes/ClienteDetalhes.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, useRef } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { Plus, Trash2 } from "lucide-react";
 
 import { Card } from "../../components/Card";
@@ -14,15 +14,29 @@ const colunas = [
     { chave: "descricao", label: "Descrição", ordenavel: false, tipo: "texto" },
 ];
 
+const dadosCli = [
+    { label: "Nome", chave: "nome", tipo: "text"},
+    { label: "Email", chave: "email", tipo: "email"},
+    { label: "Telefone", chave: "telefone", tipo: "tel"},
+    { label: "Tipo", chave: "tipo"},
+    { label: "Categoria", chave: "categoria"},
+    { label: "Resumo do Relacionamento", chave: "resumoRelacionamento"}
+];
+
 export function ClienteDetalhes() {
     const { id } = useParams();
-    const { carregando, erro, buscar, salvarNota, removerNota } = useCliente();
+    const { carregando, erro, buscar, atualizar, remover, salvarNota, removerNota } = useCliente();
     const cliente = buscar(id);
 
     const [ modalAberto, setModalAberto ] = useState();
     const [ notas, setNotas ] = useState([]);
     const [ editando, setEditando ] = useState(false);
+    const [ acaoModal, setAcaoModal ] = useState(() => () => {});
+    const [ editandoDados, setEditandoDados ] = useState(false);
     const idNotaDeletar = useRef(null);
+    const [ dadosCliente, setDadosCliente ] = useState(cliente);
+    const cliInputsRef = useRef([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
         if (cliente) setNotas(cliente.notas);
@@ -43,7 +57,35 @@ export function ClienteDetalhes() {
 
     const confirmarExcluirNota = (notaId) => {
         idNotaDeletar.current = notaId;
+
+        setAcaoModal(() => () => removerNotaLocal());
         setModalAberto(true);
+    };
+
+    const confirmarExcluirUsuario = () => {
+        setAcaoModal(() => () => {
+            remover(id);
+            navigate("/clientes");
+        });
+
+        setModalAberto(true);
+    };
+
+    const editarDados = (chave, valor) => {
+        setDadosCliente(prev => ({...prev, [chave]: valor}));
+    };
+
+    const salvarDados = (e) => {
+        e.preventDefault();
+
+        const dados = Object.fromEntries(Object.entries(cliInputsRef.current).map(([chave, el]) => [chave, el.value.trim()]));
+        atualizar(id, dados);
+        setEditandoDados(false);
+    };
+
+    const cancelarEdicaoDados = () => {
+        setEditandoDados(false);
+        setDadosCliente(cliente);
     };
 
     return (
@@ -51,64 +93,69 @@ export function ClienteDetalhes() {
             <Modal 
                 aberto={modalAberto} 
                 onFechar={() => setModalAberto(false)} 
-                mensagem="Você realmente quer remover esta nota? A ação não pode ser revertida."
+                mensagem=""
             >
                 <div className="flex gap-3">
-                    <button className="bg-green-300 p-2 rounded-sm w-1/2" onClick={removerNotaLocal}>Confirmar</button>
+                    <button className="bg-green-300 p-2 rounded-sm w-1/2" onClick={acaoModal}>Confirmar</button>
                     <button className="bg-red-300 p-2 rounded-sm w-1/2" onClick={() => setModalAberto(false)}>Cancelar</button>
                 </div>
             </Modal>
 
             <div className="flex justify-between items-start">
                 <div>
-                    <h1>{cliente.nome}</h1>
+                    <h1>{editando ? dadosCliente.nome : cliente.nome}</h1>
                     <div className="flex gap-4 mt-3 mb-1">
                         <p className="text-blue-500 bg-blue-300 w-fit px-2 rounded-sm">VIP</p>
                         <p className="text-gray-700 bg-gray-200 w-fit px-2 rounded-sm">Pessoa Física</p>
                         <p className="text-yellow-600 bg-yellow-100 w-fit px-2 rounded-sm">Alta Probabilidade</p>
                     </div>
-                    <p className="block text-gray-400">Último Contato: {cliente.ultimoContato} • Status: {cliente.status}</p>
+                    <p className="block text-gray-400">Último Contato: {dadosCliente.ultimoContato} • Status: {dadosCliente.status}</p>
                 </div>
                 <div className="flex gap-10">
-                    <button className="bg-blue-400 rounded-sm text-white p-2">Editar</button>
+                    <button className="bg-blue-400 rounded-sm text-white p-2" onClick={() => setEditandoDados(true)}>Editar</button>
                     <button className="bg-gray-200 rounded-sm p-2">Adicionar Atividade</button>
-                    <button className="bg-red-400 rounded-sm p-2 text-white">Excluir</button>
+                    <button className="bg-red-400 rounded-sm p-2 text-white" onClick={confirmarExcluirUsuario}>Excluir</button>
                 </div>
             </div>
             
             <div className="flex gap-10 my-7">
-                <Card className="grid grid-cols-2 grid-rows-auto gap-5 w-[70%]">
+                <Card className="w-[70%]">
                     <h2 className="col-span-2">Informações</h2>
 
-                    <div>
-                        <p className="text-gray-500">Nome</p>
-                        <p className="font-semibold">{cliente.nome}</p>
-                    </div>
+                    <form onSubmit={salvarDados} className="grid grid-cols-2 grid-rows-auto gap-5 ">
+                        {dadosCli.map(dado => 
+                            dado.chave != "resumoRelacionamento" ? 
+                                <div key={dado.chave}>
+                                    <p className="text-gray-500">{dado.label}</p>
+                                    <input 
+                                        className="font-semibold"
+                                        name={dado.chave}
+                                        ref={(el) => (cliInputsRef.current[dado.chave] = el)}
+                                        onChange={(e) => editarDados(e.target.name, e.target.value)}
+                                        value={dadosCliente[dado.chave]}
+                                        disabled={!editandoDados}
+                                        type={dado.tipo}
+                                        required
+                                    />
+                                </div>
+                                :
+                                <div className="col-span-2">
+                                    <h3>Resumo do Relacionamento</h3>
+                                    <p className="text-gray-500">{dadosCliente[dado.chave]}</p>
+                                </div>
+                        )}
 
-                    <div>
-                        <p className="text-gray-500">Email</p>
-                        <p className="font-semibold">{cliente.email}</p>
-                    </div>
-
-                    <div>
-                        <p className="text-gray-500">Telefone</p>
-                        <p className="font-semibold">{cliente.telefone}</p>
-                    </div>
-
-                    <div>
-                        <p className="text-gray-500">Tipo</p>
-                        <p className="font-semibold">{cliente.tipo}</p>
-                    </div>
-
-                    <div>
-                        <p className="text-gray-500">Categoria</p>
-                        <p className="font-semibold">{cliente.categoria}</p>
-                    </div>
-
-                    <div className="col-span-2">
-                        <h3>Resumo do Relacionamento</h3>
-                        <p className="text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, nemo, mollitia porro quaerat rerum accusantium sapiente numquam commodi nesciunt molestias error! Quasi, aspernatur dicta. Vero provident soluta eum sint nam?</p>
-                    </div>
+                        {editandoDados && 
+                            <div className="space-x-2">
+                                <button type="submit" className="bg-green-300 p-1 rounded-sm w-[100px] focus:bg-green-400">
+                                    Salvar
+                                </button>
+                                <button onClick={cancelarEdicaoDados} className="bg-red-300 p-1 rounded-sm w-[100px] focus:bg-red-400">
+                                    Cancelar
+                                </button>
+                            </div>
+                        }
+                    </form>
                 </Card>
 
                 <Card className="relative w-[30%] max-h-105 overflow-y-auto">
@@ -140,7 +187,7 @@ export function ClienteDetalhes() {
             </div>
 
             <h2>Histórico de Atividades</h2>
-            <Tabela dadosTabela={cliente.atividades} colunas={colunas}/>
+            <Tabela dadosTabela={dadosCliente.atividades} colunas={colunas}/>
         </>
     );
 }

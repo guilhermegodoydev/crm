b38e844cd47d7ba439a716ea0759c289b7c2e7a7
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2025-11-30 20:39:31 -0300

    finalizando responsividade da página

--- client/src/screens/clientes/ClienteDetalhes.jsx
+++ client/src/screens/clientes/ClienteDetalhes.jsx
@@ -2,31 +2,18 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { Trash2 } from "lucide-react";
 
-import { Card } from "../../components/Card";
 import { Tabela } from "../../components/tabela/TabelaBase";
 import { Modal } from "../../components/Modal";
 import { Filtro } from "../../components/Filtro";
+import { NotasViewer } from "../../components/viewers/NotasViewer";
+import { DetailsViewer } from "../../components/viewers/DetailsViewer";
+import { HeaderDetalhesCliente } from "../../components/HeaderDetalhesCliente";
 
 import { useCliente } from "../../hooks/dominio/useCliente";
 import { useAlerta } from "../../context/AlertaContexto";
 import { filtrar } from "../../utils/filtrar";
-
-import { NotasViewer } from "../../components/viewers/NotasViewer";
-
-const dadosCli = [
-    { label: "Nome", chave: "nome", tipo: "text"},
-    { label: "Email", chave: "email", tipo: "email"},
-    { label: "Telefone", chave: "telefone", tipo: "tel"},
-    { label: "Tipo", chave: "tipo", opcoes: [
-        { valor: "PJ", label: "Pessoa Jurídica" },
-        { valor: "PF", label: "Pessoa Física" }
-    ]},
-    { label: "Categoria", chave: "categoria", opcoes: [
-        { valor: "vip", label: "Vip" },
-        { valor: "regular", label: "Regular"}
-    ]},
-    { label: "Resumo do Relacionamento", chave: "resumoRelacionamento", tipo: "text"}
-];
+import { useMediaQuery } from "react-responsive";
+import { CardAtividade } from "../../components/CardAtividade";
 
 const tipoAtividades = [
     { key: "ligacao", label: "Ligação"},
@@ -34,15 +21,25 @@ const tipoAtividades = [
     { key: "reuniao", label: "Reunião"}
 ];
 
+//========================================================================================
+//
+//
+//PERMITIR ALTERAÇÃO DO CMAPO DE STATUS DO CLIENTE
+//ADICIONAR VALIDAÇÃO NO CAMPO DE TELEFONE PARA SEGUIR O FORMATO DE TELEFONE
+//
+//ADICIONAR EDIÇÃO DE ATIVIDADE DO HISTÓRICO DE ATIVIDADES
+//
+//========================================================================================
 export function ClienteDetalhes() {
     const { id } = useParams();
     const { carregando, erro, buscar, atualizar, remover, editarNota: edNota, salvarNota: slNota, removerNota: rmNota, salvarAtividade, removerAtividade } = useCliente();
     const [ cliente, setCliente ] = useState(null);
     const { exibirAlerta } = useAlerta();
+    const isMobile = useMediaQuery({ maxWidth: 1023 });
 
     const [ modal, setModal ] = useState({});
     const [ editandoDados, setEditandoDados ] = useState(false);
-    const [ dadosCliente, setDadosCliente ] = useState(cliente);
+    const [ dadosCliente, setDadosCliente ] = useState({});
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -58,6 +55,8 @@ export function ClienteDetalhes() {
     if (carregando) return <p>Carregando...</p> ;
     if (!cliente) return <p>Cliente não encontrado</p>;
     if (erro) return <p>Erro ao buscar o cliente</p>;
+
+    let anoAtual = new Date().getFullYear();
 
     const colunas = [
         { chave: "data", label: "Data", ordenavel: true, tipo: "numero" },
@@ -77,53 +76,50 @@ export function ClienteDetalhes() {
         }
     ];
 
-    console.log(dadosCliente.notas);
-    const removerNota = (notaId) => {
-        setModal({acao: null, aberto: false, mensagem: ""});
-        
+    const idadeMinima = 18;
+    const anoMinimo = anoAtual - idadeMinima;
+    let dataMax = new Date();
+    dataMax.setFullYear(anoMinimo);
+    dataMax = dataMax.toISOString().split("T")[0];
+
+    const camposFormCli = [
+        { label: "Nome", chave: "nome", valor: dadosCliente.nome, tipo: "text", autoComplete: "name"},
+        { label: "Email", chave: "email", valor: dadosCliente.email, tipo: "email", autoComplete: "email"},
+        { label: "Telefone", chave: "telefone", valor: dadosCliente.telefone, tipo: "tel", autoComplete: "tel", placeholder:"(XX) XXXX-XXXX"},
+        { label: "Data Nascimento", chave: "dataNascimento", valor: dadosCliente.dataNascimento, tipo: "date", autoComplete: "birthdate", max: dataMax},
+        { label: "Tipo", chave: "tipo", valor: dadosCliente.tipo, opcoes: [
+            { label: "Pessoa Jurídica", valor: "PJ" },
+            { label: "Pessoa Física", valor: "PF" }
+        ]},
+        { label: "Categoria", chave: "categoria" , valor: dadosCliente.categoria, opcoes: [
+            { label: "Vip", valor: "vip" },
+            { label: "Regular", valor: "regular"}
+        ]},
+        { label: "Resumo do Relacionamento", chave: "resumoRelacionamento", valor: dadosCliente.resumoRelacionamento, tipo: "text", especial: true},
+        { label: "Observação", chave: "observacao", valor: dadosCliente.observacao, tipo: "text", especial: true} 
+    ];
+    
+    const salvarDados = (dados) => {
+        if (Object.keys(dados).length == 0) {
+            setEditandoDados(false);
+            return;
+        }
+
+        let dadosFinal = dados;
+        let anoNascimento = new Date(dados.dataNascimento).getFullYear();
+
+        if (dados.dataNascimento) {
+            dadosFinal.idade = anoAtual - anoNascimento;
+        }
+
+        atualizar(id, dados);
         setDadosCliente(prev => ({
             ...prev,
-            notas: prev.notas.filter(n => n.id != notaId)
+            ...dadosFinal
         }));
-        
-        rmNota(id, notaId);
-    };
-
-    const confirmarExcluirNota = (notaId) => {
-        setModal({
-            acao: () => removerNota(notaId), 
-            aberto: true, 
-            mensagem: "Tem certeza que deseja excluir essa nota? Essa ação não pode ser revertida."
-        });
+        setEditandoDados(false);
     };
     
-
-    const confirmarExcluirUsuario = () => {
-        setModal({acao : () => {
-            remover(id);
-            navigate("/clientes");
-            exibirAlerta(`Cliente ${dadosCliente.nome} excluído com sucesso`,"sucesso");
-        }, aberto: true, mensagem: `Tem certeza que deseja excluir ${dadosCliente.nome}`});
-    };
-
-    const editarDados = (chave, valor) => {
-        setDadosCliente(prev => ({...prev, [chave]: valor}));
-    };
-
-    const salvarDados = (e) => {
-        e.preventDefault();
-        atualizar(id, dadosCliente);
-        setEditandoDados(false);
-    };
-
-    const cancelarEdicaoDados = () => {
-        setEditandoDados(false);
-        setDadosCliente(prev => ({
-            ...cliente,
-            notas: prev.notas
-        }));
-    };
-
     const confirmarExcluirAtividade = (idAtividade) => {
         setModal({
             acao: () => excluirAtividade(idAtividade),
@@ -131,7 +127,7 @@ export function ClienteDetalhes() {
             mensagem: "Tem certeza que deseja excluir esse registro?"
         });
     }
-
+    
     const excluirAtividade = (idAtividade) => {
         setDadosCliente(prev => ({
             ...prev,
@@ -140,26 +136,26 @@ export function ClienteDetalhes() {
         removerAtividade(id, idAtividade);
         setModal({acao: null, aberto: false, mensagem: ""});
     };
-
+    
     const confirmarCriarAtividade = (e) => {
         e.preventDefault();
         const formData = new FormData (e.target);
         const atividade = Object.fromEntries(formData);
-
+        
         atividade.id = crypto?.randomUUID?.() ?? `${Date.now()}-${Math.random()}`;
         salvarAtividade(id, atividade);
-
+        
         setModal({aberto: false, acao: null});
         setDadosCliente(prev => ({
             ...prev,
             atividades: [atividade, ...(prev.atividades || [])]
         }));
-
+        
     };
-
+    
     const criarAtividade = () => {
         const maxData = new Date().toISOString().split("T")[0];
-
+        
         setModal({
             aberto: true,
             acao: null,
@@ -188,7 +184,7 @@ export function ClienteDetalhes() {
                         className="resize-none mb-2 border rounded-md px-2 block w-full" 
                         placeholder="Discussão sobre integrações..."
                         required
-                    ></textarea>
+                        ></textarea>
                 </div>
 
                 <div className="flex gap-3">
@@ -198,18 +194,45 @@ export function ClienteDetalhes() {
             </form>
         });
     };
+    
+    const confirmarExcluirUsuario = () => {
+        setModal({acao : () => {
+            remover(id);
+            navigate("/clientes");
+            exibirAlerta(`Cliente ${dadosCliente.nome} excluído com sucesso`,"sucesso");
+        }, aberto: true, mensagem: `Tem certeza que deseja excluir ${dadosCliente.nome}`});
+    };
+
+    const removerNota = (notaId) => {
+        setModal({acao: null, aberto: false, mensagem: ""});
+        
+        setDadosCliente(prev => ({
+            ...prev,
+            notas: prev.notas.filter(n => n.id != notaId)
+        }));
+        
+        rmNota(id, notaId);
+    };
+
+    const confirmarExcluirNota = (notaId) => {
+        setModal({
+            acao: () => removerNota(notaId), 
+            aberto: true, 
+            mensagem: "Tem certeza que deseja excluir essa nota? Essa ação não pode ser revertida."
+        });
+    };
 
     const salvarNota = (nota) => {
         const existe = dadosCliente.notas.some(n => n.id == nota.id);
-
+        
         if (existe) 
-        {
-            setDadosCliente(prev => ({
-                ...prev,
-                notas: prev.notas.map(n => n.id == nota.id ? nota : n)
-            }));
-            edNota(id, nota);
-        }   
+            {
+                setDadosCliente(prev => ({
+                    ...prev,
+                    notas: prev.notas.map(n => n.id == nota.id ? nota : n)
+                }));
+                edNota(id, nota);
+            }   
         else {
             setDadosCliente(prev => ({
                 ...prev,
@@ -245,119 +268,71 @@ export function ClienteDetalhes() {
                 }
             </Modal>
 
-            <div className="flex justify-between items-start">
-                <div>
-                    <h1>{editandoDados ? cliente.nome : dadosCliente.nome}</h1>
-                    <div className="flex gap-4 mt-3 mb-1">
-                        <p className="text-blue-500 bg-blue-300 w-fit px-2 rounded-sm">VIP</p>
-                        <p className="text-gray-700 bg-gray-200 w-fit px-2 rounded-sm">Pessoa Física</p>
-                        <p className="text-yellow-600 bg-yellow-100 w-fit px-2 rounded-sm">Alta Probabilidade</p>
-                    </div>
-                    <p className="block text-gray-400">Último Contato: {dadosCliente.ultimoContato} • Status: {dadosCliente.status}</p>
-                </div>
-                <div className="flex gap-10">
-                    <button className="bg-blue-400 rounded-sm text-white p-2" onClick={() => setEditandoDados(true)}>Editar</button>
-                    <button className="bg-gray-200 rounded-sm p-2" onClick={criarAtividade}>Adicionar Atividade</button>
-                    <button className="bg-red-400 rounded-sm p-2 text-white" onClick={confirmarExcluirUsuario}>Excluir</button>
-                </div>
-            </div>
+            <HeaderDetalhesCliente
+                clienteParcial={{
+                    nome: dadosCliente.nome,
+                    idade: dadosCliente.idade,
+                    status: dadosCliente.status,
+                    ultimoContato: dadosCliente.ultimoContato
+                }}
+                onEditar={() => setEditandoDados(true)}
+                onAddAtividade={criarAtividade}
+                onExcluir={confirmarExcluirUsuario}
+            />
             
-            <div className="flex gap-10 my-7">
-                <Card className="w-[70%]">
-                    <h2 className="col-span-2">Informações</h2>
-
-                    <form onSubmit={(e) => salvarDados(e)} className="grid grid-cols-2 grid-rows-auto gap-5 ">
-                        {dadosCli.map(dado => 
-                            dado.chave != "resumoRelacionamento" ? 
-                                <div key={dado.chave}>
-                                    {dado.opcoes ?
-                                        <>
-                                            <label className="block text-gray-500" htmlFor={`cliente-${dado.label.toLowerCase()}`}>
-                                                {dado.label}
-                                            </label>
-                                            <select 
-                                                id={`cliente-${dado.chave}`} 
-                                                name={dado.chave} 
-                                                value={dadosCliente[dado.chave]}
-                                                onChange={(e) => editarDados(dado.chave, e.target.value)}
-                                                disabled={!editandoDados}
-                                                className={`font-semibold ${editandoDados ? "border rounded-md px-1" : "appearance-none"}`}
-                                            >
-                                                {dado.opcoes.map(o => (
-                                                    <option key={o.valor} value={o.valor}>
-                                                        {o.label}
-                                                    </option>
-                                                ))}
-                                            </select>
-                                        </>
-                                        :
-                                        <>
-                                            <label className="block text-gray-500" htmlFor={dado.chave}>{dado.label}</label>
-                                            <input 
-                                                className={`font-semibold ${editandoDados ? "border rounded-md px-1" : ""}`}
-                                                id={dado.chave}
-                                                name={dado.chave}
-                                                value={dadosCliente[dado.chave]}
-                                                disabled={!editandoDados}
-                                                onChange={(e) => editarDados(e.target.name, e.target.value)}
-                                                type={dado.tipo}
-                                                autoComplete={dado.tipo == "email" ? "email" : null}
-                                                required
-                                            />
-                                        </>
-                                    }
-                                </div>
-                                :
-                                <div key={dado.chave} className="col-span-2">
-                                    <h3>Resumo do Relacionamento</h3>
-                                    <textarea 
-                                        name={dado.chave}
-                                        value={dadosCliente[dado.chave]} 
-                                        disabled={!editandoDados}
-                                        onChange={(e) => editarDados(e.target.name, e.target.value)}
-                                        className="text-gray-500 w-full max-content resize-none"
-                                    />
-                                </div>
-                        )}
-
-                        {editandoDados && 
-                            <div className="space-x-2 col-span-2">
-                                <button type="submit" className="bg-green-300 p-1 rounded-sm w-[100px] focus:bg-green-400">
-                                    Salvar
-                                </button>
-                                <button 
-                                    type="button" 
-                                    onClick={cancelarEdicaoDados} 
-                                    className="bg-red-300 p-1 rounded-sm w-[100px] focus:bg-red-400"
-                                >
-                                    Cancelar
-                                </button>
-                            </div>
-                        }
-                    </form>
-                </Card>
+            <section className="flex flex-col lg:grid grid-cols-3 gap-10 my-7">
+                <DetailsViewer
+                    campos={camposFormCli}
+                    editando={editandoDados}
+                    onCancelar={() => setEditandoDados(false)}
+                    onSalvar={salvarDados}
+                />
 
                 <NotasViewer 
                     notas={dadosCliente.notas} 
                     onSalvar={(nota) => salvarNota(nota)}
                     onExcluir={confirmarExcluirNota}
                 />
-            </div>
+            </section>
+            
+            <section>
+                <h2>Histórico de Atividades</h2>
+                <Filtro 
+                    id="filtro-atividades" 
+                    nome="filtro-atividades" 
+                    label="Tipo" 
+                    onChange={(e) => filtrarAtividades(e.target.value)}
+                    opcoes={[
+                        {texto: "Não Filtrar", valor: ""},
+                        {texto: "Email", valor: "email"},
+                        {texto: "Ligação", valor: "ligação"},
+                        {texto: "Reunião", valor: "reunião"}
+                    ]}
+                />
 
-            <h2>Histórico de Atividades</h2>
-            <Filtro 
-                id="filtro-atividades" 
-                nome="filtro-atividades" 
-                label="Tipo" 
-                onChange={(e) => filtrarAtividades(e.target.value)}
-                opcoes={[
-                    {texto: "Não Filtrar", valor: ""},
-                    {texto: "Email", valor: "email"},
-                    {texto: "Ligação", valor: "ligação"},
-                    {texto: "Reunião", valor: "reunião"}
-                ]}
-            />
-            <Tabela dadosTabela={dadosCliente.atividades} colunas={colunas}/>
+                <div className="space-y-4 mt-4">    
+                    {isMobile ?
+                        dadosCliente.atividades.length > 0 ?
+                            dadosCliente.atividades.map(at => (
+                                <CardAtividade 
+                                    key={at.id}
+                                    id={at.id}
+                                    data={at.data}
+                                    tipo={at.tipo}
+                                    descricao={at.descricao}
+                                    onDeletar={confirmarExcluirAtividade}
+                                />
+                            ))
+                            :
+                            <p>O cliente não tem atividades registradas.</p>
+                        :
+                        <Tabela 
+                            colunas={colunas}
+                            dadosTabela={dadosCliente.atividades} 
+                        />
+                    }
+                </div>
+            </section>
         </>
     );
 }
